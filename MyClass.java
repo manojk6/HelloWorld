165c9292638be5bfaee8f328f8be0cd2203e5e74
manoj <kothamanoj288@gmail.com>
2021-09-24 17:15:48 +0530

    file modified

--- MyClass.java
+++ MyClass.java
@@ -4,10 +4,5 @@ public class MyClass {
 	  System.out.println("This is java progarm");
 	  System.out.println("Hi This is Manoj");
 	  System.out.println("Hello how are you");
-	  
-	  
-	}
+	  }
 }
-
-
-
